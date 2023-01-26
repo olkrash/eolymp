2ae21217100a0412081ca4628e142d3e97a7d234
Ilya Krasheninnikov <krasholena@gmail.com>
2023-01-26 16:00:00 +0100

    Done the task 8947

--- eolymp.cpp/8947.c
+++ eolymp.cpp/8947.c
@@ -1,17 +1,23 @@
 #import <stdio.h>
+#include <stdbool.h>
+
+bool isAsterics(const size_t x, const size_t y, const size_t n){
+    if(y % 2 == 0)
+        return true;
+    if (y % 4 == 1 && x == n - 1 || y % 4 == 3 && x == 0)
+        return true;
+    return false;
+
+}
 
 int main() {
-    int n, i, j, k = 0;
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++) {
-        if (i % 2 == 0) k++;
-        for (j = 1; j <= n; j++) {
-            if (i % 2 == 1) printf("*");
-            else if (i % 2 == 0) {
-                if ((k % 2 == 1 && j == n) || (k % 2 == 0 &&j == 1))
-                    printf("*"); else if ((k % 2 == 1 && j != n) || (k % 2 == 0 && j != 1)) printf(" ");
-            }
+    int n = 0;
+    scanf("%lu", &n);
+
+        for (size_t i = 0; i < n; ++i ){
+            for (size_t j = 0; j < n; ++j)
+            printf("%c", isAsterics(j, i, n) ? '*' : ' ');
+            printf("\n");
         }
-        printf("\n");
+    return 0;
     }
-}
